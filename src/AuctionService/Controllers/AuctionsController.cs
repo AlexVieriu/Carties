2cd17877fae1b20daa39d18ebce788ee9cbc8c1a
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-01-07 10:27:00 +0200

    18_Adding the update auction endpoint

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -53,4 +53,29 @@ public class AuctionsController : ControllerBase
 
         return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, _mapping.Map<AuctionDto>(auction));
     }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+    {
+        var auction = await _auctionDbContext.Auctions
+                                .Include(x => x.Item)
+                                .FirstOrDefaultAsync(x => x.Id == id);
+        if (auction == null)
+            return NotFound();
+
+        // TODO: check seller == current user
+
+        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+        var result = await _auctionDbContext.SaveChangesAsync() > 0;
+
+        if (!result)
+            return BadRequest("Problem saving changes");
+
+        return Ok();
+    }
 }
