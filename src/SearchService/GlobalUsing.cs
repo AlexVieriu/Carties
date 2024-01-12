13efef4fcb504578e09b90bb5e10a69d39289b9e
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-01-12 12:29:29 +0200

    26_Adding seed data

--- src/SearchService/GlobalUsing.cs
+++ src/SearchService/GlobalUsing.cs
@@ -1,3 +1,6 @@
 ﻿global using MongoDB.Driver;
 global using MongoDB.Entities;
+global using SearchService.Data;
 global using SearchService.Models;
+global using static System.Console;
+global using System.Text.Json;
